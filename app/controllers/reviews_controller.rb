6a0879ce87ad286016a0bfac68f100485e2c722f
Irma <irma.kaninda@gmail.com>
2018-06-18 18:31:24 +0200

    reviews form

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,10 +1,26 @@
 class ReviewsController < ApplicationController
   def new
+    @review = Review.new
   end
 
   def create
+    @review = Review.new(review_params)
+    if @review.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   def destroy
+    @review = Review.find(params[:id])
+    @review.destroy
+    redirect_to root_path
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:description, :rating)
   end
 end
