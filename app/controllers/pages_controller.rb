65ecb12fcb5bba341764857625d374c91ed489a4
Erland Van Reet <erlandvr@gmail.com>
2018-06-21 15:05:20 +0200

    no changes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,18 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
 
   def home
-    @artists = Artist.last(6)
+    if params[:query].present?
+         sql_query = " \
+           artists.name @@ :query \
+           OR artists.genre @@ :query \
+           OR artists.location @@ :query \
+        "
+        sql_query += "OR artists.price = #{params[:query].to_i}"
+
+      @artists = Artist.where(sql_query, query: "%#{params[:query]}%")
+    else
+      @artists = Artist.last(6)
+    end
   end
 
   def user_profile
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,8 +1,28 @@
 import "bootstrap";
 
 
-let  link = document.querySelector("#btn")
+/*let  link = document.querySelector("#btn")
 link.addEventListener('click', (event) => {
   let unhide = document.querySelector("#reviews")
   unhide.classList.toggle("hide")
 })
+*/
+
+
+// Get the modal
+let modal = document.getElementById('myModal');
+// Get the button that opens the modal
+let btn = document.getElementById("myBtn");
+// Get the <span> element that closes the modal
+let span = document.getElementsByClassName("close")[0];
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+span.onclick = function() {
+    modal.style.display = "none";
+}
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
